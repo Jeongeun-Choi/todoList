ce05a5bef81f32517d3e51317717209a459708fc
Jeongeun-Choi <cje3080@gmail.com>
2022-11-24 23:40:14 +0900

    refactor: 필요없는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { TodoInput } from "./components/TodoInput";
 import Main from "./pages/Main";
 
 function App() {
