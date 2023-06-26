63b3ac3dc01a85cf1cf35550c308a24871109813
Jeongeun-Choi <cje3080@gmail.com>
2023-06-26 19:27:02 +0900

    add: mode 변경할 수 있는 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
 import Main from "./pages/Main";
+import ReducerMain from "./pages/ReducerMain";
 
 function App() {
+  const [mode, setMode] = useState("main");
   return (
     <div className="App">
-      <Main />
+      <button
+        onClick={() => {
+          if (mode === "main") {
+            setMode("reducer");
+            return;
+          }
+          setMode("main");
+        }}
+      >
+        {mode}
+      </button>
+      {mode === "main" ? <Main /> : <ReducerMain />}
     </div>
   );
 }
