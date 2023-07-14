7c76d935764e55d16203d95c117c0eb47cb7c26e
Jeongeun-Choi <cje3080@gmail.com>
2023-07-14 14:32:32 +0900

    add: react-query 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import Main from "./pages/Main";
 import ReducerMain from "./pages/ReducerMain";
+import ZustandMain from "./pages/ZustandMain";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   const [mode, setMode] = useState("main");
+  const queryClient = new QueryClient();
   return (
     <div className="App">
-      <button
+      {/* <button
         onClick={() => {
           if (mode === "main") {
             setMode("reducer");
@@ -18,7 +22,11 @@ function App() {
       >
         {mode}
       </button>
-      {mode === "main" ? <Main /> : <ReducerMain />}
+      {mode === "main" ? <Main /> : <ReducerMain />} */}
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <ZustandMain />
+      </QueryClientProvider>
     </div>
   );
 }
